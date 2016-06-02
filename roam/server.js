943e13eb480adbe1779693c301c2eee57db46461
Kent <kentqlee@gmail.com>
2016-06-01 19:12:35 -0700

    Server handling signup requests, still writing to database when email exists

--- roam/server.js
+++ roam/server.js
@@ -16,26 +16,44 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
   console.log('I got it!');
   console.log(req.body);
-
   var data = req.body;
 
-  bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash(req.body.password, salt, function(err, hash) {
-      data.password = hash;
-      apoc.query('CREATE (newUser:User {firstName: "%firstName%", lastName: "%lastName%", password: "%password%", email: "%email%"});', data).exec().then(
-        function(res){
-        	console.log('saved to database:', res);
-    		  res.send('Nice bro');
-        },
-        function(fail){
-        	console.log('issues saving to database:', fail);
-        }
-      );
-    });
-});
+  //Check database to see if incoming email on signup already exists
+  apoc.query('MATCH (n:User {email: "%email%"}) RETURN n', { email: data.email }).exec().then(function(queryRes) {
+    console.log('RES in SERVER FILE:', queryRes[0].data.length);
+    //If there is no matching email in the database
+    if (queryRes[0].data.length === 0) {
+      //Hash password upon creation of account
+      bcrypt.genSalt(saltRounds, function(err, salt) {
+        if (err) {
+          console.log('Error generating salt', err);
+        } 
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+          if (err) {
+            console.log('Error hashing password', err);
+          }
+          data.password = hash;
+          apoc.query('CREATE (newUser:User {firstName: "%firstName%", lastName: "%lastName%", password: "%password%", email: "%email%"});', data).exec().then(
+            function(dbRes){
+              console.log('saved to database:', dbRes);
+              res.send('User created');
+            },
+            function(fail){
+              console.log('issues saving to database:', fail);
+            }
+          );
+        });
+      }); //close genssalt
+    } else {
+      res.send('Email already exists!');
+    }
 
 
-});
+
+  }); //closing 'then'
+
+
+}); //close post request
 
 app.listen(3000, function(){
 	console.log('Example app listening on port 3000!');
