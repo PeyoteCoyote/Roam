838bb7a6320bd98eb633f66dba63a650f997a9a1
Kent <kentqlee@gmail.com>
2016-06-01 17:07:34 -0700

    Password hashing function works

--- roam/server.js
+++ roam/server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var apoc = require('apoc');
+var bcrypt = require('bcrypt');
+var crypto = require('crypto');
+
+var saltRounds = 10;
 
 app.use(bodyParser.json());
 
@@ -13,19 +17,24 @@ app.post('/', function(req, res){
   console.log('I got it!');
   console.log(req.body);
 
-  // var query = 'CREATE (newUser:User {email: "'.concat(req.body.email).concat('", firstName: "').concat(req.body.firstName).concat('", lastName: "').concat(req.body.lastName).concat('", password: "').concat(req.body.password).concat('"});');
+  var data = req.body;
 
-  // console.log(query);
-  
-  apoc.query('CREATE (newUser:User {firstName: "%firstName%", lastName: "%lastName%", password: "%password%", email: "%email%"});', req.body).exec().then(
-    function(res){
-    	console.log('saved to database:', res);
-		  res.send('Nice bro');
-    },
-    function(fail){
-    	console.log('issues saving to database:', fail);
-    }
-  );
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      data.password = hash;
+      apoc.query('CREATE (newUser:User {firstName: "%firstName%", lastName: "%lastName%", password: "%password%", email: "%email%"});', data).exec().then(
+        function(res){
+        	console.log('saved to database:', res);
+    		  res.send('Nice bro');
+        },
+        function(fail){
+        	console.log('issues saving to database:', fail);
+        }
+      );
+    });
+});
+
+
 });
 
 app.listen(3000, function(){
