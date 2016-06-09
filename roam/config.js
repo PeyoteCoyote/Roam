5cde13f32afadd3ddc4a9c54b3c965d89368e761
Ben <b_chou@u.pacific.edu>
2016-06-09 09:05:46 -0700

    precommit

--- roam/config.js
+++ roam/config.js
@@ -1,2 +1,11 @@
 module.exports.accountSid = 'ACf14414620834c13269daf4c142d90360';
 module.exports.authToken = '457eab4e398ab904d3bfe3d17ac76630';
+
+const yelpKeys = {
+  consumer_key: "kvoTW_E3HCOpI9dv4dj5YQ",
+  consumer_secret: "1rO4MZ_11tpPlknTgyan3ao4Aws",
+  token: "55U6TRx19FPNnTwnnCPcVfDliFThO8px",
+  token_secret: rF29tvXZ7J8WC9BHo2WYv4wuLm0
+};
+
+module.exports = yelpKeys;
